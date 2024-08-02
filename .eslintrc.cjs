c0a7f2d73bce260b3065fb374378fa4409b0335b
Stefan Peters <stefandesu@exo.pm>
2024-08-02 13:26:25 +0200

    Revert ESLint config changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,13 +9,8 @@ module.exports = {
     "gbv",
     "gbv/vue/3",
   ],
-  parser: "@babel/eslint-parser",
   parserOptions: {
-    requireConfigFile: false,
-    babelOptions: {
-      plugins: [
-        "@babel/plugin-syntax-import-assertions",
-      ],
-    },
+    sourceType: "module",
+    ecmaVersion: 2020,
   },
 }
